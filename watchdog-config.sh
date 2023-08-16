f02c238071df3225064ac0a3b0d152f491595eb6
Ricky Saull <ricky.saull@gmail.com>
2023-08-16 10:03:43 -0400

    feat: create watchdog-config.sh
    - Enables and configures the watchdog timer
    - Performs some cleanup by removing PiCommander folder when scripts are complete.

---
+++ watchdog-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo su
+echo 'watchdog-device = /dev/watchdog' >> /etc/watchdog.conf
+echo 'watchdog-timeout = 15' >> /etc/watchdog.conf
+echo 'max-load-1 = 24' >> /etc/watchdog.conf
+echo 'RuntimeWatchdogSec=10' >> /etc/systemd/system.conf
+echo 'ShutdownWatchdogSec=10min' >> /etc/systemd/system.conf
+systemctl enable watchdog
+systemctl start watchdog
+
+# Cleanup
+(crontab -l | grep -v "/home/pi/PiCommander/scripts/watchdog-config.sh") | crontab -
+rm -rf /home/pi/PiCommander/scripts/watchdog-config.sh
